6888307a89011b478e6aa6fdece8f352b3735431
jimmyww7710 <36302780+jimmyww7710@users.noreply.github.com>
2023-09-08 15:56:15 +0800

    go through course and create a colorGenerator using React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
+import Form from './Form';
+import ColorList from './ColorList';
+import Values from 'values.js';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
+  const [colors, setColor] = useState(new Values('#f15025').all(10));
+  const addColor = (color) => {
+    try {
+      const newColor = new Values(color).all(10);
+      setColor(newColor);
+    }
+    catch (error) {
+      toast.error(error.message);
+    }
+  }
+  return (
+    <main>
+      <Form addColor={addColor} />
+      <ColorList colors={colors} />
+      <ToastContainer />
+    </main>
+  )
 };
 export default App;
